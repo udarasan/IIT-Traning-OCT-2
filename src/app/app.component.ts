ecbe793f2cc836c73214b6e98f130cd0e8216068
Udara San <udarassanjeewa@gmail.com>
2024-11-13 23:40:56 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from "./navbar/navbar.component";
+import {UserComponent} from './user/user.component';
+import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, UserComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {UserComponent} from './user/user.component';
+import {EmployeeComponent} from './employee/employee.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'user',component:UserComponent},
+  {path:'employee',component:EmployeeComponent},
+];
