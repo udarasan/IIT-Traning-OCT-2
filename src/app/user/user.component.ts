2783ca8297cbfe3bffbe9c6586e8302d32535b6c
Udara San <udarassanjeewa@gmail.com>
2024-11-15 22:36:32 +0530

    Day 8

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,46 +1,81 @@
-import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [
-    ReactiveFormsModule,HttpClientModule
+    ReactiveFormsModule, HttpClientModule, NgForOf,NgIf
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
+export class UserComponent implements OnInit{
+
+
+  users:any[]=[];
   userForm: FormGroup;
 
   constructor(private fb:FormBuilder,private http:HttpClient) {
     this.userForm=this.fb.group({
-      username:[''],
-      password:[''],
-      fName:[''],
-      sName:[''],
-      email:['']
+      username:['',Validators.required],
+      password:['',Validators.required],
+      fName:['',Validators.required],
+      sName:['',Validators.required],
+      email:['',[Validators.required]]
     })
   }
 
   onSubmit() {
-    //request send >> backend
-    console.log(this.userForm.value)
 
-    const formData=this.userForm.value
-    //send a post http request to the backend server
-    this.http.post('http://localhost:8080/api/v1/user/createUser',formData).subscribe(
+    if (this.userForm.valid){
+      //request send >> backend
+      console.log(this.userForm.value)
+
+      const formData=this.userForm.value
+      //send a post http request to the backend server
+      this.http.post('http://localhost:8080/api/v1/user/createUser',formData).subscribe(
+        (response)=>{
+          console.log("User Saved!",response);
+          this.getAllUsers();
+        },
+        (error)=>{
+          console.error('Error Saving User',error)
+        }
+      )
+    }else {
+      console.error('Form is invalid')
+    }
+  }
+
+  getAllUsers(){
+    this.http.get<any[]>('http://localhost:8080/api/v1/user/getAllUsers').subscribe(
       (response)=>{
-        console.log("User Saved!",response);
+        this.users=response;
+        console.log(this.users)
       },
       (error)=>{
-        console.error('Error Saving User',error)
+        console.error('Error fetching users',error)
       }
     )
+  }
 
-
+  ngOnInit(): void {
+    this.getAllUsers();
   }
 
 
+  deleteUser(userId:any) {
+    this.http.delete('http://localhost:8080/api/v1/user/deleteUser/'+userId).subscribe(
+      (response)=>{
+        console.log(response)
+        this.getAllUsers();
+      },
+      (error)=>{
+        console.error('Error fetching users',error)
+      }
+    )
+  }
 }
